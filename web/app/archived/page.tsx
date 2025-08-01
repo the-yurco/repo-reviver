64c133881831509f5fead7df2504d537497a5e1f
the-yurco <jurajbosy404@gmail.com>
2025-08-02 00:43:04 +0200

    feat: Create pages for the links in Navbar

---
+++ web/app/archived/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Archived = (props: Props) => {
+  return <div>Archived</div>;
+};
+
+export default Archived;
---
+++ web/app/repo/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Repo = (props: Props) => {
+  return <div>Repo</div>;
+};
+
+export default Repo;
